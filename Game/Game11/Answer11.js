2b78191ccb52c4e73c3486d7ae2ad284bd6c88e7
yukidaruma-tokubetsu <72731272+yukidaruma-tokubetsu@users.noreply.github.com>
2021-07-18 18:34:01 +0800

    Add files via upload

---
+++ Game/Game11/Answer11.js
@@ -0,0 +1,535 @@
+function btn01_1(){
+		swal("恭喜你答對了","","success");
+}
+function btn01_2(){
+		swal("不好意思你答錯了","","error");
+		var block = document.getElementById('btn01-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn01-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('Yes01');
+		block.style.visibility = 'visible';
+		Yes01.innerHTML = "Walle為「Waste Allocation Load Lifter Earth-Class」的縮寫，意思是：地球型廢棄物處理搬運機";
+}
+function btn02_1(){
+		alert("不好意思你答錯了");
+}
+function btn02_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn02-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn02-3');
+		block.style.visibility = 'hidden';
+}
+function btn03_1(){
+		alert("不好意思你答錯了");
+}
+function btn03_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn03-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn03-2');
+		block.style.visibility = 'hidden';
+}
+function btn04_1(){
+		alert("不好意思你答錯了");
+}
+function btn04_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn04-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn04-2');
+		block.style.visibility = 'hidden';
+}
+function btn05_1(){
+		alert("不好意思你答錯了");
+}
+function btn05_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn05-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn05-3');
+		block.style.visibility = 'hidden';
+}
+function btn06_1(){
+		alert("不好意思你答錯了");
+}
+function btn06_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn06-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn06-2');
+		block.style.visibility = 'hidden';
+}
+function btn07_1(){
+		alert("不好意思你答錯了");
+}
+function btn07_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn07-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn07-3');
+		block.style.visibility = 'hidden';
+}
+function btn08_1(){
+		alert("不好意思你答錯了");
+}
+function btn08_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn08-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn08-2');
+		block.style.visibility = 'hidden';
+}
+function btn09_1(){
+		alert("不好意思你答錯了");
+}
+function btn09_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn09-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn09-3');
+		block.style.visibility = 'hidden';
+}
+function btn10_1(){
+		alert("不好意思你答錯了");
+}
+function btn10_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn10-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn10-3');
+		block.style.visibility = 'hidden';
+}
+function btn11_1(){
+		alert("不好意思你答錯了");
+}
+function btn11_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn11-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn11-3');
+		block.style.visibility = 'hidden';
+}
+function btn12_1(){
+		alert("不好意思你答錯了");
+}
+function btn12_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn12-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn12-3');
+		block.style.visibility = 'hidden';
+}
+function btn13_1(){
+		alert("不好意思你答錯了");
+}
+function btn13_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn13-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn13-3');
+		block.style.visibility = 'hidden';
+}
+function btn14_1(){
+		alert("不好意思你答錯了");
+}
+function btn14_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn14-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn14-3');
+		block.style.visibility = 'hidden';
+}
+function btn15_1(){
+		alert("不好意思你答錯了");
+}
+function btn15_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn15-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn15-3');
+		block.style.visibility = 'hidden';
+}
+function btn16_1(){
+		alert("不好意思你答錯了");
+}
+function btn16_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn16-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn16-3');
+		block.style.visibility = 'hidden';
+}
+function btn17_1(){
+		alert("不好意思你答錯了");
+}
+function btn17_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn17-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn17-2');
+		block.style.visibility = 'hidden';
+}
+function btn18_1(){
+		alert("不好意思你答錯了");
+}
+function btn18_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn18-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn18-3');
+		block.style.visibility = 'hidden';
+}
+function btn19_1(){
+		alert("不好意思你答錯了");
+}
+function btn19_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn19-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn19-3');
+		block.style.visibility = 'hidden';
+}
+function btn20_1(){
+		alert("不好意思你答錯了");
+}
+function btn20_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn20-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn20-3');
+		block.style.visibility = 'hidden';
+}
+function btn21() { 
+  document.getElementById("btn21").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "腦筋急轉彎"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes21').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn22() { 
+  document.getElementById("btn22").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "玩具總動員"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes22').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn23() { 
+  document.getElementById("btn23").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "料理鼠王"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes23').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn24() { 
+  document.getElementById("btn24").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "天外奇蹟"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes24').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn25() { 
+  document.getElementById("btn25").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "勇敢傳說"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes25').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn26() { 
+  document.getElementById("btn26").addEventListener("click",function(){
+  swal("這是哪部電影的場景??", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "海底總動員"){
+    	swal("裡面隱藏的彩蛋是另外哪部電影??", {
+    		content: "input",
+  		})
+  		.then((value) => {
+    		if (value == "腦筋急轉彎"){
+      			swal("恭喜你答對了","","success");
+      			document.getElementById('Yes26').innerHTML = '海底總動員的場景，彩蛋是裡面有腦筋急轉彎的小女孩';
+    		}else if(value == ""){
+      			swal("答案不能為空白","","warning");
+    		}else{
+      			swal("不好意思你答錯了","","error");
+    		}
+  		});
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn27() { 
+  document.getElementById("btn27").addEventListener("click",function(){
+  swal("這是哪部電影的場景??", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "腦筋急轉彎"){
+    	swal("裡面隱藏的彩蛋是另外哪部電影??", {
+    		content: "input",
+  		})
+  		.then((value) => {
+    		if (value == "料理鼠王"){
+      			swal("恭喜你答對了","","success");
+      			document.getElementById('Yes27').innerHTML = '腦筋急轉彎的場景，彩蛋是裡面有料理鼠王女主角的雜誌';
+    		}else if(value == ""){
+      			swal("答案不能為空白","","warning");
+    		}else{
+      			swal("不好意思你答錯了","","error");
+    		}
+  		});
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn28() { 
+  document.getElementById("btn28").addEventListener("click",function(){
+  swal("這是哪部電影的場景??", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "料理鼠王"){
+    	swal("裡面隱藏的彩蛋是另外哪部電影??", {
+    		content: "input",
+  		})
+  		.then((value) => {
+    		if (value == "天外奇蹟"){
+      			swal("恭喜你答對了","","success");
+      			document.getElementById('Yes28').innerHTML = '料理鼠王的場景，彩蛋是裡面有天外奇蹟的狗';
+    		}else if(value == ""){
+      			swal("答案不能為空白","","warning");
+    		}else{
+      			swal("不好意思你答錯了","","error");
+    		}
+  		});
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn29() { 
+  document.getElementById("btn29").addEventListener("click",function(){
+  swal("這是哪部電影的場景??", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "天外奇蹟"){
+    	swal("裡面隱藏的彩蛋是另外哪部電影??", {
+    		content: "input",
+  		})
+  		.then((value) => {
+    		if (value == "玩具總動員"){
+      			swal("恭喜你答對了","","success");
+      			document.getElementById('Yes29').innerHTML = '天外奇蹟的場景，彩蛋是裡面有玩具總動員的抱哥';
+    		}else if(value == ""){
+      			swal("答案不能為空白","","warning");
+    		}else{
+      			swal("不好意思你答錯了","","error");
+    		}
+  		});
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn30() { 
+  document.getElementById("btn30").addEventListener("click",function(){
+  swal("這是哪部電影的場景??", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "玩具總動員"){
+    	swal("裡面隱藏的彩蛋是另外哪部電影??", {
+    		content: "input",
+  		})
+  		.then((value) => {
+    		if (value == "龍貓"){
+      			swal("恭喜你答對了","","success");
+      			document.getElementById('Yes30').innerHTML = '玩具總動員的場景，彩蛋是裡面有龍貓';
+    		}else if(value == ""){
+      			swal("答案不能為空白","","warning");
+    		}else{
+      			swal("不好意思你答錯了","","error");
+    		}
+  		});
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn31_1(){
+		alert("不好意思你答錯了");
+}
+function btn31_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn31-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn31-3');
+		block.style.visibility = 'hidden';
+}
+function btn32_1(){
+		alert("不好意思你答錯了");
+}
+function btn32_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn32-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn32-2');
+		block.style.visibility = 'hidden';
+}
+function btn33_1(){
+		alert("不好意思你答錯了");
+}
+function btn33_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn33-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn33-2');
+		block.style.visibility = 'hidden';
+}
+function btn34_1(){
+		alert("不好意思你答錯了");
+}
+function btn34_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn34-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn34-3');
+		block.style.visibility = 'hidden';
+}
+function btn35_1(){
+		alert("不好意思你答錯了");
+}
+function btn35_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn35-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn35-3');
+		block.style.visibility = 'hidden';
+}
+function btn36_1(){
+		alert("不好意思你答錯了");
+}
+function btn36_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn36-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn36-3');
+		block.style.visibility = 'hidden';
+}
+function btn37_1(){
+		alert("不好意思你答錯了");
+}
+function btn37_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn37-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn37-2');
+		block.style.visibility = 'hidden';
+}
+function btn38_1(){
+		alert("不好意思你答錯了");
+}
+function btn38_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn38-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn38-3');
+		block.style.visibility = 'hidden';
+}
+function btn39() { 
+  document.getElementById("btn39").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "海底總動員"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes39').innerHTML = '答案：海底總動員';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn40_1(){
+		alert("不好意思你答錯了");
+}
+function btn40_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn40-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn40-3');
+		block.style.visibility = 'hidden';
+}
