6f84efa37d45eeb177edc5f3f5c3b4c7196126d3
yukidaruma-tokubetsu <72731272+yukidaruma-tokubetsu@users.noreply.github.com>
2021-07-18 18:07:33 +0800

    Add files via upload

---
+++ Game/Game2/Answer2.js
@@ -0,0 +1,357 @@
+function btn1() { 
+  document.getElementById("btn1").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "奇異果"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes1').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn2() { 
+  document.getElementById("btn2").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "可樂果"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes2').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn3() { 
+  document.getElementById("btn3").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "八仙樂園"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes3').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn4() { 
+  document.getElementById("btn4").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "麗寶樂園"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes4').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn5() { 
+  document.getElementById("btn5").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "劍湖山世界"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes5').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn6() { 
+  document.getElementById("btn6").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "益可膚"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes6').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn7() { 
+  document.getElementById("btn7").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "黑人牙膏"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes7').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn8() { 
+  document.getElementById("btn8").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "足爽"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes8').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn9() { 
+  document.getElementById("btn9").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "北海鱈魚香絲"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes9').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn10() { 
+  document.getElementById("btn10").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "台灣彩券" || value == "大樂透"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes10').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn11() { 
+  document.getElementById("btn11").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "OREO"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes11').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn12() { 
+  document.getElementById("btn12").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "統一多多" || value == "多多"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes12').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn13() { 
+  document.getElementById("btn13").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "綠油精"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes13').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn14() { 
+  document.getElementById("btn14").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "肯德基"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes14').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn15() { 
+  document.getElementById("btn15").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "Extra口香糖" || value == "口香糖"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes15').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn16() { 
+  document.getElementById("btn16").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "麥香"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes16').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn17() { 
+  document.getElementById("btn17").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "純喫茶"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes17').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn18() { 
+  document.getElementById("btn18").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "波蜜果菜汁"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes18').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn19() { 
+  document.getElementById("btn19").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "古道超no油切"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes19').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn20() { 
+  document.getElementById("btn20").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "斯斯"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes20').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn21() { 
+  document.getElementById("btn21").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "髮膠"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes21').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
