ceed05c0015168eebbb2a0dbf13c69b6ea73f041
yukidaruma-tokubetsu <72731272+yukidaruma-tokubetsu@users.noreply.github.com>
2021-07-18 18:24:11 +0800

    Add files via upload

---
+++ Game/Game6/Answer6.js
@@ -0,0 +1,425 @@
+function btn01() { 
+  document.getElementById("btn01").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "Thank You"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes01').innerHTML = '答案：Thank You';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn02() { 
+  document.getElementById("btn02").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "I Don't Know"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes02').innerHTML = "答案：I Don't Know";
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn03() { 
+  document.getElementById("btn03").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "I Know Right"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes03').innerHTML = '答案：I Know Right?';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn04() { 
+  document.getElementById("btn04").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "Let Me Know"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes04').innerHTML = '答案：Let Me Know';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn05() { 
+  document.getElementById("btn05").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "Also Know As"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes05').innerHTML = '答案：Also Know As';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn06() { 
+  document.getElementById("btn06").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "Be Right Back"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes06').innerHTML = '答案：Be Right Back';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn07() { 
+  document.getElementById("btn07").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "As Soon As Possible"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes07').innerHTML = '答案：As Soon As Possible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn08() { 
+  document.getElementById("btn08").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "Talk To You Later"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes08').innerHTML = '答案：Talk To You Later';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn09() { 
+  document.getElementById("btn09").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "Welcome Back"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes09').innerHTML = '答案：Welcome Back';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn10() { 
+  document.getElementById("btn10").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "Just Kidding"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes10').innerHTML = '答案：Just Kidding';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn11() { 
+  document.getElementById("btn11").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "To Be Honest"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes11').innerHTML = '答案：To Be Honest';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn12() { 
+  document.getElementById("btn12").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "In My Opinion"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes12').innerHTML = '答案：In My Opinion';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn13() { 
+  document.getElementById("btn13").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "Thanks God It's Friday"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes13').innerHTML = "答案：Thanks God It's Friday";
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn14() { 
+  document.getElementById("btn14").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "Laugh Out Loud"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes14').innerHTML = '答案：Laugh Out Loud';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn15() { 
+  document.getElementById("btn15").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "Laugh My Ass Off"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes15').innerHTML = '答案：Laugh My Ass Off';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn16() { 
+  document.getElementById("btn16").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "What The Hell"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes16').innerHTML = '答案：What The Hell';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn17() { 
+  document.getElementById("btn17").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "Get The Fuck Out"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes17').innerHTML = '答案：Get The Fuck Out';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn18() { 
+  document.getElementById("btn18").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "I Don't Give A Fuck"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes18').innerHTML = "答案：I Don't Give A Fuck";
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn19() { 
+  document.getElementById("btn19").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "Jesus Fucking Christ"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes19').innerHTML = '答案：Jesus Fucking Christ';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn20() { 
+  document.getElementById("btn20").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "Do It Yourself"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes20').innerHTML = '答案：Do It Yourself';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn21() { 
+  document.getElementById("btn21").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "You Only Live Once"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes21').innerHTML = '答案：You Only Live Once';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn22() { 
+  document.getElementById("btn22").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "For Your Information"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes22').innerHTML = '答案：For Your Information';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn23() { 
+  document.getElementById("btn23").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "In Real Life"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes23').innerHTML = '答案：In Real Life';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn24() { 
+  document.getElementById("btn24").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "Date Of Birth"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes24').innerHTML = '答案：Date Of Birth';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn25() { 
+  document.getElementById("btn25").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "Good Game"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes25').innerHTML = '答案：Good Game';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
