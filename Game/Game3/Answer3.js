10a293cd8989d385510a023af3f8036851d45587
yukidaruma-tokubetsu <72731272+yukidaruma-tokubetsu@users.noreply.github.com>
2021-07-18 18:09:40 +0800

    Add files via upload

---
+++ Game/Game3/Answer3.js
@@ -0,0 +1,255 @@
+function btn01() { 
+  document.getElementById("btn01").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "大象"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes01').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn02() { 
+  document.getElementById("btn02").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "板南線"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes02').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn03() { 
+  document.getElementById("btn03").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "汽水"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes03').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn04() { 
+  document.getElementById("btn04").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "跑跑卡丁車"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes04').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn05() { 
+  document.getElementById("btn05").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "蚊子"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes05').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn06() { 
+  document.getElementById("btn06").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "火柴"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes06').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn07() { 
+  document.getElementById("btn07").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "電影院"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes07').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn08() { 
+  document.getElementById("btn08").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "船"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes08').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn09() { 
+  document.getElementById("btn09").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "粉筆"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes09').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn10() { 
+  document.getElementById("btn10").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "乒乓球"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes10').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn11() { 
+  document.getElementById("btn11").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "雨刷"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes11').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn12() { 
+  document.getElementById("btn12").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "爆爆王"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes12').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn13() { 
+  document.getElementById("btn13").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "瓦斯爐"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes13').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn14() { 
+  document.getElementById("btn14").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "平交道"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes14').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
+function btn15() { 
+  document.getElementById("btn15").addEventListener("click",function(){
+  swal("請輸入答案", {
+    content: "input",
+  })
+  .then((value) => {
+    if (value == "響尾蛇"){
+      swal("恭喜你答對了","","success");
+      document.getElementById('Yes15').style.visibility = 'visible';
+    }else if(value == ""){
+      swal("答案不能為空白","","warning");
+    }else{
+      swal("不好意思你答錯了","","error");
+    }
+  });
+});
+}
