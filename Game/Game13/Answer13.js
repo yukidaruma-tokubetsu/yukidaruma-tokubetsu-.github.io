8407b4f25b784527974caa4d5ba41807a3a0a772
yukidaruma-tokubetsu <72731272+yukidaruma-tokubetsu@users.noreply.github.com>
2021-07-18 18:36:17 +0800

    Add files via upload

---
+++ Game/Game13/Answer13.js
@@ -0,0 +1,300 @@
+function btn01_1(){
+		alert("不好意思你答錯了");
+}
+function btn01_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn01-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn01-3');
+		block.style.visibility = 'hidden';
+}
+function btn02_1(){
+		alert("不好意思你答錯了");
+}
+function btn02_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn02-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn02-2');
+		block.style.visibility = 'hidden';
+}
+function btn03_1(){
+		alert("不好意思你答錯了");
+}
+function btn03_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn03-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn03-2');
+		block.style.visibility = 'hidden';
+}
+function btn04_1(){
+		alert("不好意思你答錯了");
+}
+function btn04_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn04-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn04-3');
+		block.style.visibility = 'hidden';
+}
+function btn05_1(){
+		alert("不好意思你答錯了");
+}
+function btn05_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn05-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn05-3');
+		block.style.visibility = 'hidden';
+}
+function btn06_1(){
+		alert("不好意思你答錯了");
+}
+function btn06_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn06-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn06-2');
+		block.style.visibility = 'hidden';
+}
+function btn07_1(){
+		alert("不好意思你答錯了");
+}
+function btn07_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn07-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn07-2');
+		block.style.visibility = 'hidden';
+}
+function btn08_1(){
+		alert("不好意思你答錯了");
+}
+function btn08_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn08-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn08-2');
+		block.style.visibility = 'hidden';
+}
+function btn09_1(){
+		alert("不好意思你答錯了");
+}
+function btn09_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn09-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn09-3');
+		block.style.visibility = 'hidden';
+}
+function btn10_1(){
+		alert("不好意思你答錯了");
+}
+function btn10_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn10-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn10-2');
+		block.style.visibility = 'hidden';
+}
+function btn11_1(){
+		alert("不好意思你答錯了");
+}
+function btn11_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn11-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn11-3');
+		block.style.visibility = 'hidden';
+}
+function btn12_1(){
+		alert("不好意思你答錯了");
+}
+function btn12_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn12-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn12-3');
+		block.style.visibility = 'hidden';
+}
+function btn13_1(){
+		alert("不好意思你答錯了");
+}
+function btn13_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn13-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn13-3');
+		block.style.visibility = 'hidden';
+}
+function btn14_1(){
+		alert("不好意思你答錯了");
+}
+function btn14_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn14-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn14-3');
+		block.style.visibility = 'hidden';
+}
+function btn15_1(){
+		alert("不好意思你答錯了");
+}
+function btn15_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn15-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn15-3');
+		block.style.visibility = 'hidden';
+}
+function btn16_1(){
+		alert("不好意思你答錯了");
+}
+function btn16_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn16-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn16-2');
+		block.style.visibility = 'hidden';
+}
+function btn17_1(){
+		alert("不好意思你答錯了");
+}
+function btn17_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn17-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn17-3');
+		block.style.visibility = 'hidden';
+}
+function btn18_1(){
+		alert("不好意思你答錯了");
+}
+function btn18_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn18-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn18-3');
+		block.style.visibility = 'hidden';
+}
+function btn19_1(){
+		alert("不好意思你答錯了");
+}
+function btn19_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn19-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn19-3');
+		block.style.visibility = 'hidden';
+}
+function btn20_1(){
+		alert("不好意思你答錯了");
+}
+function btn20_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn20-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn20-3');
+		block.style.visibility = 'hidden';
+}
+function btn21_1(){
+		alert("不好意思你答錯了");
+}
+function btn21_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn21-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn21-3');
+		block.style.visibility = 'hidden';
+}
+function btn22_1(){
+		alert("不好意思你答錯了");
+}
+function btn22_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn22-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn22-3');
+		block.style.visibility = 'hidden';
+}
+function btn23_1(){
+		alert("不好意思你答錯了");
+}
+function btn23_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn23-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn23-3');
+		block.style.visibility = 'hidden';
+}
+function btn24_1(){
+		alert("不好意思你答錯了");
+}
+function btn24_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn24-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn24-2');
+		block.style.visibility = 'hidden';
+}
+function btn25_1(){
+		alert("不好意思你答錯了");
+}
+function btn25_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn25-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn25-3');
+		block.style.visibility = 'hidden';
+}
+function btn26_1(){
+		alert("不好意思你答錯了");
+}
+function btn26_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn26-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn26-3');
+		block.style.visibility = 'hidden';
+}
+function btn27_1(){
+		alert("不好意思你答錯了");
+}
+function btn27_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn27-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn27-3');
+		block.style.visibility = 'hidden';
+}
+function btn28_1(){
+		alert("不好意思你答錯了");
+}
+function btn28_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn28-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn28-3');
+		block.style.visibility = 'hidden';
+}
+function btn29_1(){
+		alert("不好意思你答錯了");
+}
+function btn29_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn29-1');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn29-2');
+		block.style.visibility = 'hidden';
+}
+function btn30_1(){
+		alert("不好意思你答錯了");
+}
+function btn30_2(){
+		alert("恭喜你答對了");
+		var block = document.getElementById('btn30-2');
+		block.style.visibility = 'hidden';
+		var block = document.getElementById('btn30-3');
+		block.style.visibility = 'hidden';
+}
